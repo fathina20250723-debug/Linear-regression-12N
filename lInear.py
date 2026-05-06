10cac02537b9d5df432497549d702533ab191f1b
fathina20250723-debug <fathina20250723@miis.ae>
2026-05-06 12:53:51 +0400

    Update lInear.py

--- lInear.py
+++ lInear.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.linear_model import train_test_split
 from sklearn.model_selection import LinearRegression
 df = pd.read_csv("students_scores.csv")
 X = df[['hours studied']]
